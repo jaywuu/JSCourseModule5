6d81e7987059cbdfb33133b5619ee0f7a66b14ca
jaywuu <jaywu.civil@gmail.com>
2018-09-10 01:52:49 -0700

    Progress tracker

--- starter/script.js
+++ starter/script.js
@@ -99,7 +99,7 @@ console.log(arrayCalc(arrayCalc(years, calcAge), isFullAge));
 */
 
 // Function returning Functions
-
+/*
 function interviewQuestion(job) {
   if (job === 'designer') {
     return function(name) {
@@ -122,3 +122,134 @@ var teacherQuestion = interviewQuestion('teacher');
 teacherQuestion('John');
 
 interviewQuestion('designer')('mark')
+*/
+/*
+function game() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+}
+*/
+/*
+(function() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+console.log(score);
+*/
+
+// Closures
+/*
+function retirement(retirementAge) {
+  var str = ' years left until retirement';
+  return function(yearOfBirth) {
+    var age = 2018 - yearOfBirth;
+    console.log(retirementAge - age + str);
+  }
+}
+
+var retirementUS = retirement(66)(1996);
+*/
+/////////////////////////////////////////
+// Bind, call and apply
+/*
+var john = {
+  name: 'John',
+  age: 26,
+  job: 'teacher',
+  presentation: function(style, timeOfDay) {
+    if (style === 'formal') {
+      console.log('Good ' + timeOfDay + ', ladies and gentlemen ! I\'m ' +
+                  this.name + ' I\'m a ' + this.job);
+
+    }
+    else if (style === 'friendly') {
+      console.log('Whats up' + ' I\'m ' + this.name + ' in the ' + timeOfDay);
+    }
+  }
+};
+*/
+/*
+john.presentation('formal', 'morning');
+
+var emily = {
+  name: 'Emily',
+  age: 35,
+  job: 'designer'
+};
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+john.presentation.apply(emily, ['friendly', 'afternoon']);
+var johnFriendly = john.presentation.bind(john, 'friendly');
+johnFriendly('morning');
+*/
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+var gameOn = true;
+var Question = function(question, answers, correctAns) {
+  this.question = question;
+  this.answers = answers;
+  this.correctAns = correctAns;
+};
+
+Question.prototype.displayQuestion = function() {
+  console.log(this.question);
+};
+
+Question.prototype.displayAnswers = function() {
+  for (var i=0; i<this.answers.length; i++) {
+    console.log(i + ': ' + this.answers[i]);
+  }
+};
+
+Question.prototype.checkAnwer = function(input) {
+    if (input == this.correctAns) {
+      console.log("Congrats ! ");
+      gameOn = false;
+    }
+    else {
+      console.log("Wrong answer !");
+    }
+}
+
+var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+                      ['Yes', 'No'],
+                      0);
+
+var q2 = new Question('What is the name of this course\'s teacher?',
+                      ['John', 'Micheal', 'Jonas'],
+                      2);
+
+var q3 = new Question('What does best describe coding?',
+                      ['Boring', 'Hard', 'Fun', 'Tediuos'],
+                      2);
+var questions = [q1, q2, q3];
+//var q = questions[0];
+//console.log(q);
+
+//////////////
+while (gameOn == true) {
+  var n = Math.floor(Math.random() * questions.length);
+  console.log(n);
+  questions[n].displayQuestion();
+  questions[n].displayAnswers();
+  input = prompt("Please answer the above question ?");
+  questions[n].checkAnwer(input);
+}
